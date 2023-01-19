2137bb69428125dc7b9a22caf0ce5d4a4a7e624d
Pandurang <manoj.dhanorkar@gmail.com>
2023-01-19 11:12:41 +0000

    GoLang Operator

---
+++ apis/aws/v1/awsvmschedulerstopad_types.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AWSVMSchedulerStopadSpec defines the desired state of AWSVMSchedulerStopad
+type AWSVMSchedulerStopadSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// // Foo is an example field of AWSVMSchedulerStopad. Edit awsvmschedulerstopad_types.go to remove/update
+	// Foo string `json:"foo,omitempty"`
+	// Comma separated list of AWS instance ids which will be scheduled by CR
+	InstanceIds string `json:"instanceIds"`
+
+	// Schedule period for the CronJob.
+	// This spec allows you to setup the stop schedule for VM
+	StopSchedule string `json:"stopSchedule"`
+
+	Image string `json:"image"`
+}
+
+// AWSVMSchedulerStopadStatus defines the observed state of AWSVMSchedulerStopad
+type AWSVMSchedulerStopadStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	VMStopStatus string `json:"vmStopStatus"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// AWSVMSchedulerStopad is the Schema for the awsvmschedulerstopads API
+type AWSVMSchedulerStopad struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AWSVMSchedulerStopadSpec   `json:"spec,omitempty"`
+	Status AWSVMSchedulerStopadStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// AWSVMSchedulerStopadList contains a list of AWSVMSchedulerStopad
+type AWSVMSchedulerStopadList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AWSVMSchedulerStopad `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AWSVMSchedulerStopad{}, &AWSVMSchedulerStopadList{})
+}
